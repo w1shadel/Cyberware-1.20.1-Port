2996fd4ff027592347b05b198a6bf556bb8dbe27
w1shadel <kik.souta@gmail.com>
2025-12-15 16:51:24 +0900

    a

--- src/main/java/com/Maxwell/cyber_ware_port/Common/CyberwareTabState.java
+++
@@ -1,5 +0,0 @@
-package com.Maxwell.cyber_ware_port.Common;
-
-public class CyberwareTabState {
-    public static int currentPage = 0;
-}
--- src/main/java/com/Maxwell/cyber_ware_port/Common/Item/CyberSkullType.java
+++
@@ -1,8 +0,0 @@
-package com.Maxwell.cyber_ware_port.Common.Item;
-
-import net.minecraft.world.level.block.SkullBlock;
-
-public enum CyberSkullType implements SkullBlock.Type {
-    CYBER_WITHER_SKELETON
-
-}
