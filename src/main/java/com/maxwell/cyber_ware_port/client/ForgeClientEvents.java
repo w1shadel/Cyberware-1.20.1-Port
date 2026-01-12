dcb17f55ecce29621c4cdea2bc7d9b26dd1e0c39
w1shadel <kik.souta@gmail.com>
2026-01-12 13:02:29 +0900

    a

--- src/main/java/com/maxwell/cyber_ware_port/client/ForgeClientEvents.java
+++ src/main/java/com/maxwell/cyber_ware_port/client/ForgeClientEvents.java
@@ -5,6 +5,8 @@ import com.maxwell.cyber_ware_port.client.upgrades.cyberEye.CyberwareMenuScreen;
 import com.maxwell.cyber_ware_port.common.capability.CyberwareCapabilityProvider;
 import com.maxwell.cyber_ware_port.common.capability.CyberwareUserData;
 import com.maxwell.cyber_ware_port.common.item.base.BodyPartType;
+import com.maxwell.cyber_ware_port.common.item.base.CyberwareItem;
+import com.maxwell.cyber_ware_port.common.item.base.CyberwareSlotType;
 import com.maxwell.cyber_ware_port.common.item.base.ICyberware;
 import com.maxwell.cyber_ware_port.common.network.A_PacketHandler;
 import com.maxwell.cyber_ware_port.common.network.DoubleJumpPacket;
@@ -12,9 +14,11 @@ import com.maxwell.cyber_ware_port.init.ModBlocks;
 import com.maxwell.cyber_ware_port.init.ModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -25,9 +29,11 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.List;
+import java.util.Set;
 
 @SuppressWarnings("removal")
 @Mod.EventBusSubscriber(modid = CyberWare.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
@@ -38,46 +44,153 @@ public class ForgeClientEvents {
     public static void onItemTooltip(ItemTooltipEvent event) {
         ItemStack stack = event.getItemStack();
         Item item = stack.getItem();
+        List<Component> tooltip_block = event.getToolTip();
+        if (item == ModBlocks.RADIO_KIT_BLOCK.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.radio_kit")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.COMPONENT_BOX.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.component_box")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.RADIO_TOWER_CORE.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.radio_tower_core")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.SCANNER.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.scanner")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.RADIO_TOWER_COMPONENT.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.radio_component")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.radio_component2")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.radio_component3")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.CHARGER.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.charger")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.charger2")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.charger3")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.SURGERY_CHAMBER.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.surgery_chamber")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.surgery_chamber2")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.BLUEPRINT_CHEST.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.blueprint_chest")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.CYBERWARE_WORKBENCH.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.cyberware_workbench")
+                    .withStyle(ChatFormatting.GRAY));
+        }
+        if (item == ModBlocks.ROBO_SURGEON.get().asItem()) {
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.robo_surgeon")
+                    .withStyle(ChatFormatting.GRAY));
+            tooltip_block.add(Component.translatable("tooltip.cyber_ware_port.robo_surgeon2")
+                    .withStyle(ChatFormatting.GRAY));
+        }
         List<Component> tooltip = event.getToolTip();
-        addBlockTooltips(item, tooltip);
         if (stack.hasTag() && stack.getTag().getBoolean("cyberware_ghost")) {
             Component name = tooltip.isEmpty() ? stack.getHoverName() : tooltip.get(0);
             tooltip.clear();
             tooltip.add(name);
-            tooltip.add(Component.empty());
-            tooltip.add(Component.translatable("cyberware.tooltip.ghost.remove")
-                    .withStyle(ChatFormatting.RED));
+            tooltip.add(Component.translatable("cyberware.tooltip.ghost.remove").withStyle(ChatFormatting.RED));
             return;
         }
-    }
-
-    private static void addBlockTooltips(Item item, List<Component> tooltip) {
-        if (item == ModBlocks.RADIO_KIT_BLOCK.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.radio_kit").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.COMPONENT_BOX.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.component_box").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.RADIO_TOWER_CORE.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.radio_tower_core").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.SCANNER.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.scanner").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.RADIO_TOWER_COMPONENT.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.radio_component").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.radio_component2").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.radio_component3").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.CHARGER.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.charger").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.charger2").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.charger3").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.SURGERY_CHAMBER.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.surgery_chamber").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.surgery_chamber2").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.BLUEPRINT_CHEST.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.blueprint_chest").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.CYBERWARE_WORKBENCH.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.cyberware_workbench").withStyle(ChatFormatting.GRAY));
-        } else if (item == ModBlocks.ROBO_SURGEON.get().asItem()) {
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.robo_surgeon").withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("tooltip.cyber_ware_port.robo_surgeon2").withStyle(ChatFormatting.GRAY));
+        if (stack.getItem() instanceof CyberwareItem cyberware) {
+            ResourceLocation regName = ForgeRegistries.ITEMS.getKey(cyberware);
+            if (regName != null && regName.getPath().contains("body_part")) {
+                return;
+            }
+            boolean isShiftDown = Screen.hasShiftDown();
+            if (!isShiftDown) {
+                tooltip.add(Component.empty());
+                Component keyName = Component.translatable("key.keyboard.shift");
+                tooltip.add(Component.translatable("cyberware.tooltip.shiftPrompt", keyName)
+                        .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
+                return;
+            }
+            ResourceLocation registryName = ForgeRegistries.ITEMS.getKey(cyberware);
+            if (registryName != null) {
+                String path = registryName.getPath();
+                String key = "cyberware.tooltip." + path;
+                tooltip.add(Component.empty());
+                tooltip.add(Component.translatable(key).withStyle(ChatFormatting.GRAY));
+            }
+            tooltip.add(Component.empty());
+            if (cyberware.canToggle(stack)) {
+                boolean isActive = cyberware.isActive(stack);
+                Component statusText = Component.translatable(isActive ? "cyberware.gui.active.enable" : "cyberware.gui.active.disable")
+                        .withStyle(isActive ? ChatFormatting.GREEN : ChatFormatting.RED);
+                tooltip.add(Component.translatable("cyberware.tooltip.status", statusText)
+                        .withStyle(ChatFormatting.WHITE));
+            }
+            if (cyberware.hasEnergyProperties(stack)) {
+                int consumption = cyberware.getEnergyConsumption(stack);
+                if (consumption > 0) {
+                    tooltip.add(Component.translatable("cyberware.tooltip.powerConsumption", consumption)
+                            .withStyle(ChatFormatting.RED));
+                }
+                int generation = cyberware.getEnergyGeneration(stack);
+                if (generation > 0) {
+                    tooltip.add(Component.translatable("cyberware.tooltip.powerProduction", generation)
+                            .withStyle(ChatFormatting.GREEN));
+                }
+                int storage = cyberware.getEnergyStorage(stack);
+                if (storage > 0) {
+                    tooltip.add(Component.translatable("cyberware.tooltip.capacity", storage)
+                            .withStyle(ChatFormatting.AQUA));
+                }
+                int eventCost = cyberware.getEventConsumption(stack);
+                if (eventCost > 0) {
+                    tooltip.add(Component.translatable("cyberware.tooltip.eventCost", eventCost)
+                            .withStyle(ChatFormatting.RED));
+                }
+            }
+            if (cyberware.getMaxInstallAmount(stack) > 1) {
+                tooltip.add(Component.translatable("cyberware.tooltip.maxInstall", cyberware.getMaxInstallAmount(stack))
+                        .withStyle(ChatFormatting.BLUE));
+            }
+            tooltip.add(Component.translatable("cyberware.tooltip.essence", cyberware.getEssenceCost(stack))
+                    .withStyle(ChatFormatting.DARK_PURPLE));
+            Set<Item> reqs = cyberware.getPrerequisites(stack);
+            if (!reqs.isEmpty()) {
+                tooltip.add(Component.empty());
+                tooltip.add(Component.translatable("cyberware.tooltip.requires").withStyle(ChatFormatting.AQUA));
+                for (Item req : reqs) {
+                    tooltip.add(Component.literal(" - ").append(req.getName(new ItemStack(req))).withStyle(ChatFormatting.GRAY));
+                }
+            }
+            Set<Item> incompatibles = cyberware.getIncompatibleItems(stack);
+            if (!incompatibles.isEmpty()) {
+                tooltip.add(Component.empty());
+                tooltip.add(Component.translatable("cyberware.tooltip.incompatible").withStyle(ChatFormatting.RED));
+                for (Item incompatible : incompatibles) {
+                    tooltip.add(Component.literal(" - ")
+                            .append(incompatible.getName(new ItemStack(incompatible)))
+                            .withStyle(ChatFormatting.GRAY));
+                }
+            }
+            CyberwareSlotType slotType = CyberwareSlotType.fromId(cyberware.getSlot(stack));
+            if (slotType != null) {
+                tooltip.add(Component.translatable("cyberware.tooltip.slot", slotType.getDisplayName())
+                        .withStyle(ChatFormatting.GRAY));
+            }
+            if (cyberware.isPristine(stack)) {
+                tooltip.add(Component.translatable("cyberware.quality.manufactured")
+                        .withStyle(ChatFormatting.AQUA));
+            } else {
+                tooltip.add(Component.translatable("cyberware.quality.scavenged")
+                        .withStyle(ChatFormatting.RED));
+            }
         }
     }
 
