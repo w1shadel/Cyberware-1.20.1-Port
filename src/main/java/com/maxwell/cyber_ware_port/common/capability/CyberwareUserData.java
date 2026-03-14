aea41f695988e368c8fc876325dce00481dea566
w1shadel <205571913+w1shadel@users.noreply.github.com>
2026-03-15 08:39:31 +0900

    oo

--- src/main/java/com/maxwell/cyber_ware_port/common/capability/CyberwareUserData.java
+++ src/main/java/com/maxwell/cyber_ware_port/common/capability/CyberwareUserData.java
@@ -391,10 +391,10 @@ public class CyberwareUserData implements INBTSerializable<CompoundTag>, IEnergy
 
     private void updateBodyStatus() {
         CyberwareBodyStatus status = new CyberwareBodyStatus(installedCyberware);
-        this.hasCyberLeftArm = status.getArmCount() >= 1;
-        this.hasCyberRightArm = status.getArmCount() >= 2;
-        this.hasCyberLeftLeg = status.getLegCount() >= 1;
-        this.hasCyberRightLeg = status.getLegCount() >= 2;
+        this.hasCyberLeftArm = status.getCyberArmCount() >= 1;
+        this.hasCyberRightArm = status.getCyberArmCount() >= 2;
+        this.hasCyberLeftLeg = status.getCyberLegCount() >= 1;
+        this.hasCyberRightLeg = status.getCyberLegCount() >= 2;
     }
 
     public void syncToClient(ServerPlayer player) {
