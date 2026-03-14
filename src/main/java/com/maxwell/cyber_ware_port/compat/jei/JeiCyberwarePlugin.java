65457865b2f04198823ecc4c8f7be79f83ac63dd
w1shadel <205571913+w1shadel@users.noreply.github.com>
2026-03-15 07:58:53 +0900

    Stable implementation of JEI/EMI plugins following the 1.21.1 implementation.

---
+++ src/main/java/com/maxwell/cyber_ware_port/compat/jei/JeiCyberwarePlugin.java
@@ -0,0 +1,46 @@
+package com.maxwell.cyber_ware_port.compat.jei;
+
+import com.maxwell.cyber_ware_port.CyberWare;
+import com.maxwell.cyber_ware_port.init.ModBlocks;
+import com.maxwell.cyber_ware_port.init.ModRecipes;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+@JeiPlugin
+public class JeiCyberwarePlugin implements IModPlugin {
+    public static final ResourceLocation UID = new ResourceLocation(CyberWare.MODID, "jei_plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return UID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new AssemblyRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new EngineeringRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager rm = Minecraft.getInstance().level.getRecipeManager();
+
+        // 1.20.1ではRecipeHolderを介さず直接リストを取得
+        registration.addRecipes(AssemblyRecipeCategory.RECIPE_TYPE, rm.getAllRecipesFor(ModRecipes.ASSEMBLY_TYPE.get()));
+        registration.addRecipes(EngineeringRecipeCategory.RECIPE_TYPE, rm.getAllRecipesFor(ModRecipes.ENGINEERING_TYPE.get()));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.CYBERWARE_WORKBENCH.get()), AssemblyRecipeCategory.RECIPE_TYPE);
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.CYBERWARE_WORKBENCH.get()), EngineeringRecipeCategory.RECIPE_TYPE);
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.SCANNER.get()), EngineeringRecipeCategory.RECIPE_TYPE);
+    }
+}
