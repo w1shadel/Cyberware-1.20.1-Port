c369f6290b9a4c14b4b57568fb4704007649fbb9
w1shadel <205571913+w1shadel@users.noreply.github.com>
2026-05-05 00:10:39 +0900

    file moving

--- src/main/java/com/maxwell/cyber_ware_port/client/screen/ScannerScreen.java
+++ src/main/java/com/maxwell/cyber_ware_port/client/screen/ScannerScreen.java
@@ -1,4 +1,4 @@
-package com.maxwell.cyber_ware_port.client.screen.scanner;
+package com.maxwell.cyber_ware_port.client.screen;
 
 import com.maxwell.cyber_ware_port.CyberWare;
 import com.maxwell.cyber_ware_port.common.container.ScannerMenu;
